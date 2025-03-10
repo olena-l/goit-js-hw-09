d4696182638651ebeb6123cea84d157ead6e493d
olena-l <fiaccola@ukr.net>
2025-03-10 17:24:45 +0100

    add js hw 9

---
+++ src/js/2-form.js
@@ -0,0 +1,47 @@
+const STORAGE_KEY = 'feedback-form-state';
+const formData = { email: '', message: '' };
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input');
+const msgTextarea = form.querySelector('textarea');
+
+populateForm();
+
+form.addEventListener('input', onTextareaInput);
+
+form.addEventListener('submit', onFormSubmit);
+
+function onTextareaInput(event) {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = '';
+  formData.message = '';
+}
+
+function populateForm() {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedFormData) {
+    const { email, message } = JSON.parse(savedFormData);
+
+    emailInput.value = email;
+    msgTextarea.value = message;
+  }
+}
